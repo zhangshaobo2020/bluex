991da96c98fa8df18466b5e60391a08dd1cc2481
zhangshaobo2020 <zhangshaobo2020@github.com>
2025-08-04 10:48:33 +0800

    项目初始化

---
+++ src/main/java/com/zsb/bluex/BluexApplication.java
@@ -0,0 +1,13 @@
+package com.zsb.bluex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsb/bluex/BluexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsb.bluex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

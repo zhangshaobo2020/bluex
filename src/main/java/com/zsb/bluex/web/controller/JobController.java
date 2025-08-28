c8c20529d20ed67ee7b1aa4a37cbcb5bf52a27f3
zhangshaobo2020 <zhangshaobo2020@github.com>
2025-08-28 22:13:02 +0800

    基本的动态注册和测试

--- src/main/java/com/zsb/bluex/web/controller/JobController.java
+++ src/main/java/com/zsb/bluex/web/controller/JobController.java
@@ -1,39 +1,78 @@
 package com.zsb.bluex.web.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zsb.bluex.job.JobRegistry;
 import com.zsb.bluex.model.entity.BluexJob;
 import com.zsb.bluex.model.entity.BluexJobSearch;
+import com.zsb.bluex.model.service.BluexJobService;
+import com.zsb.bluex.utils.CommonUtil;
+import com.zsb.bluex.utils.MybatisPlusUtils;
 import com.zsb.bluex.web.Pagination;
 import com.zsb.bluex.web.WebResult;
-import com.zsb.bluex.web.service.JobService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/job")
 public class JobController {
 
     @Resource
-    private JobService jobService;
+    private BluexJobService bluexJobService;
+    @Resource
+    private JobRegistry jobRegistry;
 
     @GetMapping("/jobPage")
     public WebResult<Pagination<BluexJob>> jobPage(HttpServletRequest request, BluexJobSearch search) {
-        return WebResult.success(jobService.jobPage(new Pagination<>(request), search));
+        Pagination<BluexJob> pagination = new Pagination<>(request);
+        QueryWrapper<BluexJob> queryWrapper = MybatisPlusUtils.getQueryWrapper(search);
+        Page<BluexJob> page = bluexJobService.page(pagination.generate(), queryWrapper);
+        return WebResult.success(pagination.transfer(page));
     }
 
     @GetMapping("/jobDetail")
     public WebResult<BluexJob> jobDetail(@RequestParam String jobNo) {
-        return WebResult.success(jobService.jobDetail(jobNo));
+        return WebResult.success(bluexJobService.getById(jobNo));
     }
 
     @PostMapping("/jobSubmit")
+    @Transactional(rollbackFor = Throwable.class)
     public WebResult<BluexJob> jobSubmit(@RequestBody BluexJob bluexJob) {
-        return WebResult.success(jobService.jobSubmit(bluexJob));
+
+        if (StringUtils.isBlank(bluexJob.getJobNo())) {
+            // 新增
+            bluexJob.setJobNo(CommonUtil.generateUUID());
+            bluexJob.setCreateTime(LocalDateTime.now());
+            bluexJob.setUpdateTime(LocalDateTime.now());
+            bluexJobService.save(bluexJob);
+        } else {
+            bluexJob.setUpdateTime(LocalDateTime.now());
+            bluexJobService.updateById(bluexJob);
+        }
+        return jobDetail(bluexJob.getJobNo());
     }
 
     @PostMapping("/jobDelete")
+    @Transactional(rollbackFor = Throwable.class)
     public WebResult<String> jobDelete(@RequestParam String jobNo) {
-        return WebResult.success(jobService.jobDelete(jobNo));
+        bluexJobService.removeById(jobNo);
+        return WebResult.success("OK");
+    }
+
+    @PostMapping("/registerJob")
+    public WebResult<String> registerJob(@RequestParam String jobNo) {
+        jobRegistry.registerJob(bluexJobService.getById(jobNo));
+        return WebResult.success("OK");
+    }
+
+    @PostMapping("/unregisterJob")
+    public WebResult<String> unregisterJob(@RequestParam String jobNo) {
+        jobRegistry.unregisterJob(jobNo);
+        return WebResult.success("OK");
     }
 }
--- src/main/java/com/zsb/bluex/web/controller/ProgramController.java
+++ src/main/java/com/zsb/bluex/web/controller/ProgramController.java
@@ -1,40 +1,67 @@
 package com.zsb.bluex.web.controller;
 
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.zsb.bluex.model.entity.BluexProgram;
 import com.zsb.bluex.model.entity.BluexProgramSearch;
+import com.zsb.bluex.model.service.BluexProgramService;
+import com.zsb.bluex.utils.CommonUtil;
+import com.zsb.bluex.utils.MybatisPlusUtils;
 import com.zsb.bluex.web.Pagination;
 import com.zsb.bluex.web.WebResult;
 import com.zsb.bluex.web.mapping.TaskGraph;
-import com.zsb.bluex.web.service.ProgramService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/program")
 public class ProgramController {
 
     @Resource
-    private ProgramService programService;
+    private BluexProgramService bluexProgramService;
 
     @GetMapping("/programPage")
     public WebResult<Pagination<BluexProgram>> programPage(HttpServletRequest request, BluexProgramSearch search) {
-        return WebResult.success(programService.programPage(new Pagination<>(request), search));
+        Pagination<BluexProgram> pagination = new Pagination<>(request);
+        QueryWrapper<BluexProgram> queryWrapper = MybatisPlusUtils.getQueryWrapper(search);
+        Page<BluexProgram> page = bluexProgramService.page(pagination.generate(), queryWrapper);
+        return WebResult.success(pagination.transfer(page));
     }
 
     @GetMapping("/programDetail")
     public WebResult<BluexProgram> programDetail(@RequestParam String programNo) {
-        return WebResult.success(programService.programDetail(programNo));
+        return WebResult.success(bluexProgramService.getById(programNo));
     }
 
     @PostMapping("/programSubmit")
+    @Transactional(rollbackFor = Throwable.class)
     public WebResult<BluexProgram> programSubmit(@RequestBody TaskGraph taskGraph) {
-        return WebResult.success(programService.programSubmit(taskGraph));
+        BluexProgram program = taskGraph.getProgram();
+        program.setJsonContent(JSON.toJSONString(taskGraph.getGraph()));
+
+        if (StringUtils.isBlank(program.getProgramNo())) {
+            // 新增
+            program.setProgramNo(CommonUtil.generateUUID());
+            program.setCreateTime(LocalDateTime.now());
+            program.setUpdateTime(LocalDateTime.now());
+            bluexProgramService.save(program);
+        } else {
+            program.setUpdateTime(LocalDateTime.now());
+            bluexProgramService.updateById(program);
+        }
+        return programDetail(program.getProgramNo());
     }
 
     @PostMapping("/programDelete")
+    @Transactional(rollbackFor = Throwable.class)
     public WebResult<String> programDelete(@RequestParam String programNo) {
-        return WebResult.success(programService.programDelete(programNo));
+        bluexProgramService.removeById(programNo);
+        return WebResult.success("OK");
     }
 }

4c4c365cb02f6b77eefef94fb8bdee9d4d46ba43
zhangshaobo2020 <zhangshaobo2020@github.com>
2025-09-04 16:17:09 +0800

    修改ws路径

--- src/main/java/com/zsb/bluex/web/socket/WebSocketConfig.java
+++ src/main/java/com/zsb/bluex/web/socket/WebSocketConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SimpleTextWebSocketHandler(), "/ws")
+        registry.addHandler(new SimpleTextWebSocketHandler(), "/ws/console")
                 .setAllowedOrigins("*");  // 允许所有跨域访问，生产环境请根据需求配置
     }
 }
